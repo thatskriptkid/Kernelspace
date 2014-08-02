c5765affea88537ff0ac7b23e5596da36ae525e7
thatskriptkid <baurgun@gmail.com>
2014-08-02 06:18:12 -0400

    add struct class,struct device,device.h,err.h,refactoring cdev_create,module_exit,add MODULE_*

--- simple_module.c
+++ simple_module.c
@@ -4,78 +4,99 @@
 #include <linux/fs.h> //alloc_chrdev_region(), struct file_operations
 #include <linux/types.h> // dev_t
 #include <linux/cdev.h> // cdev structure
+#include <linux/device.h> //class_create()
+#include <linux/err.h> //for error codes (ERR_PTR)
 
 #define SUCCESS 0
 #define DEV_RANGE 300 // device number range 
-#define DEV_NAME "my_char_device" //name of our device
 
+static struct device *device;//the basic device structure 
+static char *DEV_NAME="my_char_device"; //name of our device
 static dev_t DEV_NUM; // device MAJOR and minor numbers
-static struct inode *inode; //represent char device internally, in kernel
-static struct cdev *my_cdev; //need for inode structure
+static struct inode *chr_inode; //represent char device internally, in kernel
+static struct cdev *my_cdev; //need for inode structure, represent a character device within the kernel
+static struct class *device_class;
 static struct file_operations fops = // function pointers on actions on our device
 {
 	.owner=THIS_MODULE, //prevent module unloading
 	
 };
-struct file *filep;//represent file in kernel
+static void cdev_create(void);// create cdev structure
+
+static void chrdev_init(void)
+{
+	device_class=class_create(THIS_MODULE,DEV_NAME);// create a struct class structure 
+	
+	if(alloc_chrdev_region(&DEV_NUM,0,1,DEV_NAME)!=SUCCESS)
+		{
+			printk(KERN_WARNING "register_chrdev_region() failed\n"); //allocate char device number dynamically
+			goto unreg;
+		}
+		
+	if(!DEV_NUM)
+		{
+			printk(KERN_WARNING "DEV_NUM was not set\n");
+			goto unreg;
+		}
+	
+	cdev_create();
+	
+	device=device_create(device_class,NULL,DEV_NUM,NULL,DEV_NAME);//creates a device and registers it with sysfs
+	
+		if (IS_ERR(device)) 
+		{
+			printk(KERN_WARNING "device_create() failed\n");
+			goto cdev_del;
+		}
+		
+		
+	unreg:
+		unregister_chrdev_region(DEV_NUM,1);//free device number 
+		
+	cdev_del:
+		cdev_del(my_cdev);
+	
+}
 
 static void cdev_create(void)
 {
-	my_cdev=cdev_alloc();
-	my_cdev->ops=&fops;
-	my_cdev->owner=THIS_MODULE;
+	if((my_cdev=cdev_alloc())==NULL)
+		printk(KERN_WARNING "cdev_alloc() failed\n");
+	
 	
 	cdev_init(my_cdev,&fops);
+	
+	my_cdev->ops=&fops;
+	my_cdev->owner=THIS_MODULE;
 	
 	if((cdev_add(my_cdev,DEV_NUM,1))<0)
 		{
 			printk(KERN_WARNING "cdev_add() failed\n");
 			cdev_del(my_cdev);
 		}
-	else inode->i_cdev=my_cdev; //add cdev structure to our inode
 	
 }
 
-static void chrdev_init(void)
-{
-	if(alloc_chrdev_region(&DEV_NUM,0,DEV_RANGE,DEV_NAME)!=SUCCESS)
-		{
-			printk(KERN_WARNING "register_chrdev_region() failed\n"); //allocate char device number dynamically
-			goto unreg;
-		}
-	else printk(KERN_WARNING "register_chrdev_region() successful\n");	
-	
-	if(!DEV_NUM)
-		{
-			printk(KERN_WARNING "DEV_NUM was not set\n");
-			goto unreg;
-		}
-	else printk(KERN_WARNING "DEV_NUM is set\n");	
-	
-	/*
-	filep->f_mode=FMODE_READ|FMODE_WRITE;
-	filep->f_op=&fops;
-	*/
-	cdev_create();
-	
-		
-	unreg:
-		unregister_chrdev_region(DEV_NUM,300);//free device number 
-	
-}
+
 
 static int __init finit(void)
 {
 	chrdev_init();
+
 	return 0;
 }
 
 static void __exit fexit(void)
 {
-	
+	cdev_del(my_cdev);
+	unregister_chrdev_region(DEV_NUM,1);//free device number 
+	class_destroy(device_class); //destroys a struct class structure 
 	
 }
 
 module_init(finit);
 module_exit(fexit);
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("thatskriptkid");
+MODULE_DESCRIPTION("module that create char device");

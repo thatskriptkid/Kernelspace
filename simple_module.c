77995b0cd935a39cd62c3efbeb9080af086e0ee4
thatskriptkid <baurgun@gmail.com>
2014-08-03 04:59:52 -0400

    Refactoring,change *my_cdev to cdev, change cdev->owner to cdev.owner (it produced kernel error), change error handling This is first commit after which you can test simple_module.ko. After insmod you are able to see newly created char device in /proc/devices. Rmmod also work properly (I hope) :)

--- simple_module.c
+++ simple_module.c
@@ -1,4 +1,7 @@
-//Error recovery is sometimes best handled with the goto statement. (c) Linux Device Driver, 3rd edtiion,Chapter 2.
+/*
+ * Error recovery is sometimes best handled with the goto statement. (c) Linux Device Driver, 3rd edtiion,Chapter 2.
+ * Try to code according to https://www.kernel.org/doc/Documentation/CodingStyle
+*/
 
 #include <linux/kernel.h> //printk()
 #include <linux/module.h> //THIS_MODULE
@@ -16,7 +19,7 @@ static struct device *device;//the basic device structure
 static char *DEV_NAME="my_char_device"; //name of our device
 static dev_t DEV_NUM; // device MAJOR and minor numbers
 static struct inode *chr_inode; //represent char device internally, in kernel
-static struct cdev *my_cdev; //need for inode structure, represent a character device within the kernel
+static struct cdev my_cdev; //need for inode structure, represent a character device within the kernel
 static struct class *device_class;
 static struct file_operations fops = // function pointers on actions on our device
 {
@@ -33,40 +36,40 @@ static void chrdev_release(void);//release all resources related to our char dev
 
 static int __init finit(void) //main() analogue
 {
-	if(chrdev_init())
+	if(chrdev_init()) 
 		printk(KERN_WARNING "chrdev_init() failed\n");
-	else 
+	else  
 		printk(KERN_INFO "chrdev_init() successful\n");
 	return 0;
 }
 
 static int chrdev_init(void)
 {
-	device_class=class_create(THIS_MODULE,DEV_NAME);// create a struct class structure 
+	device_class=class_create(THIS_MODULE,DEV_NAME);
 	
 	if (IS_ERR(device_class)) {
 		printk(KERN_WARNING "class_create() failed\n");
+		return 1;
+	}
+	
+	if(alloc_chrdev_region(&DEV_NUM,0,1,DEV_NAME)!=SUCCESS) {//allocate char device number dynamically
+		printk(KERN_WARNING "alloc_chrdev_region() failed\n"); 
 		goto class_destroy;
 	}
 	
-	if(alloc_chrdev_region(&DEV_NUM,0,1,DEV_NAME)!=SUCCESS) {
-			printk(KERN_WARNING "register_chrdev_region() failed\n"); //allocate char device number dynamically
-			goto cdev_del;
-	}
-	
 	if(cdev_create()) {
-			printk(KERN_WARNING "cdev_create() failed\n");
-			goto unreg;
+		printk(KERN_WARNING "cdev_create() failed\n");
+		goto unreg;
 	}
 	
 	device=device_create(device_class,NULL,DEV_NUM,NULL,DEV_NAME);//creates a device and registers it with sysfs
 	
 	if (IS_ERR(device)) {
-			printk(KERN_WARNING "device_create() failed\n");
-			goto device_release; 
+		printk(KERN_WARNING "device_create() failed\n");
+		goto cdev_del;
 	}
 		
-	chr_inode->i_cdev=my_cdev;//add cdev struct to our inode
+	//chr_inode->i_cdev=my_cdev;//add cdev struct to our inode
 	
 	return 0; //chrdev_init() success
 	
@@ -74,30 +77,26 @@ static int chrdev_init(void)
 		unregister_chrdev_region(DEV_NUM,1);
 		
 	cdev_del:
-		cdev_del(my_cdev);
+		cdev_del(&my_cdev);
 		
 	class_destroy:
 		class_destroy(device_class); 
-	
-	device_release:
-		device_destroy(device_class,DEV_NUM);		
-	
+			
 	return 1;
-	
+		
 }
 
 static int cdev_create(void)
 {
-	cdev_init(my_cdev,&fops);
+	cdev_init(&my_cdev,&fops);
 	
-	my_cdev->ops=&fops;
-	my_cdev->owner=THIS_MODULE;
+	my_cdev.ops=&fops;
+	my_cdev.owner=THIS_MODULE;
 	
-	if((cdev_add(my_cdev,DEV_NUM,1))<0)
-		{
-			printk(KERN_WARNING "cdev_add() failed\n");
-			return 1;
-		}
+	if((cdev_add(&my_cdev,DEV_NUM,1))<0) {
+		printk(KERN_WARNING "cdev_add() failed\n");
+		return 1;
+	}
 		
 	return 0;
 	
@@ -106,9 +105,9 @@ static int cdev_create(void)
 static void chrdev_release(void)
 {
 	device_destroy(device_class,DEV_NUM);	//remove a device that was created with device_create
-	cdev_del(my_cdev);
+	cdev_del(&my_cdev);
 	unregister_chrdev_region(DEV_NUM,1); //free device number 
-	class_destroy(device_class);  //destroy a struct class structure;
+	class_destroy(device_class);  //destroy a struct class structure
 }
 
 static void __exit fexit(void)

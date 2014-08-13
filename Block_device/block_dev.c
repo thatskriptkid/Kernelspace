90eb448ee86b24812c658752188583c84e3f85ce
thatskriptkid <baurgun@gmail.com>
2014-08-13 09:38:40 -0400

    change Makefile, refactoring

--- Block_device/block_dev.c
+++ Block_device/block_dev.c
@@ -8,21 +8,13 @@
 #define SUCCESS 0
 #define KERNEL_SECTOR_SIZE 512
 
-static struct gendisk        *gd;                        //kernel's representation of individual device 
-static int    	  			 major_number;
+static struct gendisk        *device;                        //kernel's representation of individual device 
+static int    	  			  major_number;
 static char   	 			 *device_name="my_block_device";
 static struct request_queue  *queue;
-static spinlock_t 			 lock; 
-static int                   nsectors = 1024;                 /* How big the drive is */
-static int					 hardsect_size = 512;
-
-static struct my_block_device {
-	unsigned long size;
-	spinlock_t lock;
-	u8 *data;
-	struct gendisk *gd;
-} device;
-
+static spinlock_t 			  lock; 
+static int                    nsectors = 1024;                 /* How big the drive is */
+static int					  hardsect_size = 512;
 
 static int  gendisk_create(void);
 static void my_request(struct request_queue *queue); /*This function is called whenever the kernel believes it is time 
@@ -34,7 +26,7 @@ static struct block_device_operations file_op =
 {
 	.owner=THIS_MODULE,
 	.open=NULL,
-	.release=NULL,
+	.release=NULL
 	
 };
 
@@ -46,7 +38,6 @@ static int block_dev_init(void)
 		printk(KERN_WARNING "register_blkdev() failed\n");
 		return 1;
 	}
-	else printk(KERN_WARNING "register_blkdev() success\n");
 	
 	spin_lock_init(&lock);
 	
@@ -56,25 +47,21 @@ static int block_dev_init(void)
 		printk(KERN_WARNING "blk_init_queue() failed\n");
 		goto unregister;
 	}
-	else printk(KERN_WARNING "blk_init_queue() success\n"); 
 	
 	if(gendisk_create()) {
 		printk(KERN_WARNING "gendisk_create() failed\n"); 
 		goto free_queue;
 	}
-	else printk(KERN_WARNING "gendisk_create() success\n"); 
-	
-	
-	
 	
 	return SUCCESS;
 	
-	unregister:
-		unregister_blkdev(major_number,device_name);
-	free_gendisk:
-		del_gendisk(device);
 	free_queue:
 		blk_cleanup_queue(queue);
+	unregister:
+		unregister_blkdev(major_number,device_name);
+	/*free_gendisk:
+		del_gendisk(device);*/
+	
 		
 	return 1;	
 	
@@ -108,7 +95,7 @@ void my_request(struct request_queue *queue)
 	while((req=blk_fetch_request(queue))!=NULL) {
 		
 		 /*tells us whether we are looking at a 
-		  *filesystem request — one that moves blocks of data.
+		  *filesystem request
 		  */
 		  
 		if (req->cmd_type!=REQ_TYPE_FS) {
@@ -116,8 +103,8 @@ void my_request(struct request_queue *queue)
 			blk_end_request(req,-1,10); 
 			continue;
 		}
-		
-		my_block_device_transfer(
+		else
+			printk(KERN_WARNING "there is request!\n");
 		blk_end_request(req,SUCCESS,10);
 	}
 }
@@ -133,9 +120,7 @@ static void __exit dev_exit(void)
 {
 	del_gendisk(device);
 	blk_cleanup_queue(queue);
-	
 	unregister_blkdev(major_number,device_name);
-	
 }
 
 module_init(dev_init);

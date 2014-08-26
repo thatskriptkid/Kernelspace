2dedd80e63f89c757677e805cd75561f7d993510
thatskriptkid <baurgun@gmail.com>
2014-08-26 14:36:35 -0400

    add file read function

--- IO/kernel_IO.c
+++ IO/kernel_IO.c
@@ -10,17 +10,37 @@
 #include <linux/fs.h>
 
 #define SUCCESS 0 
+#define RNG_NAME "/dev/urandom"
 
-MODULE_LICENSE("GPL");
+struct file  *filp;
+ssize_t      bytes_read;
+char __user  *buff;
+ssize_t      count;
+loff_t       offset = 0;
+
+void file_close(struct file* file);
 
 static int __init finit(void)
 {
-	struct file *filp=NULL;
+	filp = filp_open(RNG_NAME,O_RDONLY,0); // in fs/open.c 
 	
-	filp = filp_open("/dev/urandom",O_RDONLY,0); // in fs/open.c 
+	if(IS_ERR(filp))
+		printk(KERN_WARNING "filp_open() failed!\n");
+	else
+		printk(KERN_WARNING "filp_open() success!\n");
+		
+	bytes_read = vfs_read(filp,buff,2,&offset);
 	
+	printk(KERN_WARNING "bytes_read = %d \n",((int)bytes_read));
+	
+	file_close(filp);
 	
 	return 0;
+}
+
+void file_close(struct file* file) 
+{
+    filp_close(file, NULL);
 }
 
 static void __exit fexit(void)
@@ -32,5 +52,6 @@ module_init(finit);
 module_exit(fexit);
 
 //traditionally in the end
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("thatskriptkid");
 MODULE_DESCRIPTION("IO in kernel space!!! :-)");

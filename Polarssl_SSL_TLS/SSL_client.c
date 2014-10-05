d286f87c62ce5f985afdc57f83e475f867a8cfec
thatskriptkid <baurgun@gmail.com>
2014-10-05 13:49:03 -0400

    add .gitignore,provide headers and lib files for net communication,add klog

--- Polarssl_SSL_TLS/SSL_client.c
+++ Polarssl_SSL_TLS/SSL_client.c
@@ -1,7 +1,20 @@
+#include "net.h"
+//#include "ssl.h"
+
 #define SUCCESS 0
+#define REQUEST "GET /stefan/testfile.txt HTTP/1.1\r\nHost: thunked.org\r\n\r\n"
+
 
 static int __init finit(void)
 {
+	int sock;
+	int ret;
+	
+	if((ret=net_connect(&sock,"thunked.org",443))!=0)
+		printk(KERN_WARNING "net_connect() failed\n");
+	else
+		printk(KERN_WARNING "net_connect() success\n");
+		
 	return SUCCESS;
 }
 

d166eec0fa3af8c61b218cbe6f8cfef16691b4de
thatskriptkid <baurgun@gmail.com>
2014-10-26 08:51:00 -0700

    lol

---
+++ Polarssl_SSL_TLS/client/polarssl_kernel_support.h
@@ -0,0 +1,25 @@
+#pragma once 
+
+/* Linux Kernel headers */
+ 
+#include <linux/kernel.h>  // printk()
+#include <linux/module.h>  // THIS_MODULE
+#include <linux/init.h>    // __init(),__exit()
+#include <linux/slab.h>    // kmalloc()
+#include <linux/string.h>
+#include <linux/syscalls.h>
+#include <linux/fcntl.h>
+#include <asm/uaccess.h>
+#include <linux/types.h>    //umode_t
+#include <linux/fs.h>
+#include <asm/segment.h>
+
+/* Linux Kernel headers */ 
+
+
+#include "klog.h"
+
+#define __SUBCOMPONENT__ "kernel_polarssl"
+#define __LOGNAME__ "kernel_polarssl.log"
+
+
